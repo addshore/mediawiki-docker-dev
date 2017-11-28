2ba1eb6d093dd141f4f4321a3464af94fa4a6aa6
Timo Tijhof <krinklemail@gmail.com>
2017-11-28 14:23:48 -0800

    Use sub directory for MediaWiki
    Instead localhost/index.php, it is now localhost/mediawiki/index.php.
    
    Keep the localhost/ redirect working by adding a wrapper in the
    document root that loads MediaWiki. This is essentially the
    same technique WordPress popularises and works better than
    writing a rewrite-rule, because we'd have to maintain those
    for each web server backend separately, whereas right now our
    setup is webserver agnostic. The docker-compose can be tied
    to Linux distribution, web server type, or PHP version. They
    provide the same base interface from webdevops/*.
    
    Update Travis CI:
    
    * Follow the redirect in its cURL request, currently it was only
      showing the headers for the 301 redirect from the document root.
    
    * Change -i to -I so that it keeps showing headers only instead
      of full HTML. Previously -i allowed it to show a body, but there
      was no body for the redirect. Now that we follow the redirect,
      switch to -I so that we still don't dump the body.
    
    Fixes addshore/mediawiki-docker-dev#28.

---
+++ config/mediawiki/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This file is copied by ./up to web://var/www/index.php.
+ *
+ * This entry point allows MediaWiki to handle requests for "/".
+ */
+
+require __DIR__ . '/mediawiki/index.php';

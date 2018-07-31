13a137a3392e3addd24fdf4f328d5d900b341273
addshore <addshore@users.noreply.github.com>
2018-07-31 17:27:36 +0100

    Fix typo #2, rom -> from

--- config/mysql/slave/entrypoint.sh
+++ config/mysql/slave/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # On Windows if we mount the file directly it will end up having 777 permissions and mysql won't read the config
-# So instead mount to a temporary directory and copy rom there, then chmoding to 0444
+# So instead mount to a temporary directory and copy from there, then chmoding to 0444
 rm -rf /etc/mysql/conf.d/*.cnf
 cp /tmp/mwdd/slave.cnf /etc/mysql/conf.d/slave.cnf
 chmod 0444 /etc/mysql/conf.d/slave.cnf

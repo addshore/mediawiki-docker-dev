fe8bcfb156e2bd31d123cefff20854e1e36de5ae
addshore <addshore@users.noreply.github.com>
2018-08-03 09:49:22 +0100

    Copy and chmod mysql configs for windows... (#59)
    * Copy and chmod mysql configs for windows...
    * set +x for mysql entrypoints
    * Add comments to the mysql entrypoints

---
+++ config/mysql/master/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# On Windows if we mount the file directly it will end up having 777 permissions and mysql won't read the config
+# So instead mount to a temporary directory and copy from there, then chmoding to 0444
+rm -rf /etc/mysql/conf.d/*.cnf
+cp /tmp/mwdd/master.cnf /etc/mysql/conf.d/master.cnf
+chmod 0444 /etc/mysql/conf.d/master.cnf
+
+# Then execute the regular mysql / mariadb entrypoint
+/usr/local/bin/docker-entrypoint.sh $@

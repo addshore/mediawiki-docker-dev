b8536c41cc5eda8b2722afcc65c433c3e3052654
Kosta Harlan <kharlan@wikimedia.org>
2019-02-15 14:14:10 -0500

    Update docs, use setup.sh in Travis CI

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+set -eu
 
-mkdir -p ~/src/
-git clone https://gerrit.wikimedia.org/r/mediawiki/core ~/src/mediawiki
-git clone https://gerrit.wikimedia.org/r/mediawiki/skins/Vector ~/src/mediawiki/skins/Vector
-cd ~/src/mediawiki
+mkdir -p $INSTALL_DIR
+git clone https://gerrit.wikimedia.org/r/mediawiki/core $INSTALL_DIR/mediawiki
+git clone https://gerrit.wikimedia.org/r/mediawiki/skins/Vector $INSTALL_DIR/mediawiki/skins/Vector
+cd $INSTALL_DIR/mediawiki
 docker run -it --rm --user $(id -u):$(id -g) -v ~/.composer:/tmp -v $(pwd):/app docker.io/composer install
 touch LocalSettings.php
 cat > LocalSettings.php <<EOL

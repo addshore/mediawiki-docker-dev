de6b5000326728a6f2eb5587de37ca2af4f890a8
Kosta Harlan <kharlan@wikimedia.org>
2019-02-22 11:30:46 -0500

    Quote install dir variable

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 
-mkdir -p $INSTALL_DIR
-git clone https://gerrit.wikimedia.org/r/mediawiki/core $INSTALL_DIR/mediawiki
-git clone https://gerrit.wikimedia.org/r/mediawiki/skins/Vector $INSTALL_DIR/mediawiki/skins/Vector
-cd $INSTALL_DIR/mediawiki
+mkdir -p "$INSTALL_DIR"
+git clone https://gerrit.wikimedia.org/r/mediawiki/core "$INSTALL_DIR"/mediawiki
+git clone https://gerrit.wikimedia.org/r/mediawiki/skins/Vector "$INSTALL_DIR"/mediawiki/skins/Vector
+cd "$INSTALL_DIR"/mediawiki
 docker run -it --rm --user $(id -u):$(id -g) -v ~/.composer:/tmp -v $(pwd):/app docker.io/composer install
 touch LocalSettings.php
 cat > LocalSettings.php <<EOL

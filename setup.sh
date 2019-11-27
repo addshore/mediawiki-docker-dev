ed260c6abe82c32b90ada12a9bbacbf897a9f018
addshore <addshorewiki@gmail.com>
2019-11-27 18:57:53 +0000

    travis: use shallow clone for mw and skin for speed

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ set -eu
 mkdir -p "$INSTALL_DIR"
 [[ ! -d $HOME/.composer ]] && mkdir $HOME/.composer
 
-git clone https://gerrit.wikimedia.org/r/mediawiki/core "$INSTALL_DIR"/mediawiki
-git clone https://gerrit.wikimedia.org/r/mediawiki/skins/Vector "$INSTALL_DIR"/mediawiki/skins/Vector
+git clone --depth 1 https://gerrit.wikimedia.org/r/mediawiki/core "$INSTALL_DIR"/mediawiki
+git clone --depth 1 https://gerrit.wikimedia.org/r/mediawiki/skins/Vector "$INSTALL_DIR"/mediawiki/skins/Vector
 
 cd "$INSTALL_DIR"/mediawiki
 docker run -it --rm --user $(id -u):$(id -g) -v $HOME/.composer:/tmp -v $(pwd):/app composer install --ignore-platform-reqs

c19cff5916e87b6012b490689fde3c879cc50f63
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2020-05-15 10:56:51 +0100

    Set DOCKER_MW_PATH on setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,12 @@ mkdir -p "$INSTALL_DIR"
 git clone --depth 1 https://gerrit.wikimedia.org/r/mediawiki/core "$INSTALL_DIR"/mediawiki
 git clone --depth 1 https://gerrit.wikimedia.org/r/mediawiki/skins/Vector "$INSTALL_DIR"/mediawiki/skins/Vector
 
-cd "$INSTALL_DIR"/mediawiki
+
+DOCKER_MW_PATH="$INSTALL_DIR"/mediawiki
+
+echo "DOCKER_MW_PATH=$DOCKER_MW_PATH" >> local.env
+
+cd "$DOCKER_MW_PATH"
 docker run -it --rm --user $(id -u):$(id -g) -v $HOME/.composer:/tmp -v $(pwd):/app composer install --ignore-platform-reqs
 touch LocalSettings.php
 cat > LocalSettings.php <<EOL
